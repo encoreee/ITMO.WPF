93f3bc57f97a3646c45962aa6c9f07da429aabfc
AlexR <alexraykov2000@gmail.com>
2020-12-10 23:42:46 +0300

    3_2

--- ITMO.WPF.NCommand/MainWindow.xaml.cs
+++ ITMO.WPF.NCommand/MainWindow.xaml.cs
@@ -23,6 +23,30 @@ namespace ITMO.WPF.NCommand
         public MainWindow()
         {
             InitializeComponent();
+            CommandBinding abinding = new CommandBinding();
+            abinding.Command = CustomCommands.Launch;
+            abinding.Executed += new ExecutedRoutedEventHandler(Launch_Handler);
+            abinding.CanExecute += new CanExecuteRoutedEventHandler(LaunchEnabled_Handler);
+            this.CommandBindings.Add(abinding);
+
+           
+        }
+
+        private void Launch_Handler(object sender, ExecutedRoutedEventArgs e)
+        {
+            richTextBox.AppendText(textBox.Text);
+            using (System.IO.StreamWriter writer = new System.IO.StreamWriter("log.txt", true))
+            {
+                writer.WriteLine("Внесено {0}: {1} ", textBox.Text, DateTime.Now.ToShortDateString() + ", время: " +
+                DateTime.Now.ToLongTimeString());
+
+                writer.Flush();
+            }
+        }
+
+        private void LaunchEnabled_Handler(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = (bool)checkBox.IsChecked;
         }
     }
 }
