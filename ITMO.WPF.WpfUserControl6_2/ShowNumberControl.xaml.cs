efcf9dc8a01fb9fdab19bf5342bfcdfd964940ed
Александр Райков <alexraykov2000@gmail.com>
2020-12-15 16:28:44 +0300

    7_2

---
+++ ITMO.WPF.WpfUserControl6_2/ShowNumberControl.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ITMO.WPF.WpfUserControl6_2
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControl1.xaml
+    /// </summary>
+    public partial class ShowNumberControl : UserControl
+    {
+
+        public ShowNumberControl()
+        {
+            InitializeComponent();
+        }
+
+
+        public static readonly DependencyProperty CurrentNumberProperty = DependencyProperty.Register("CurrentNumber", typeof(int),
+                                                                          typeof(ShowNumberControl),
+                                                                          new UIPropertyMetadata(100,
+                                                                          new PropertyChangedCallback(CurrentNumberChanged)),
+                                                                          new ValidateValueCallback(ValidateCurrentNumber));
+        //private int currNumber = 0;
+        //public int CurrentNumber
+        //{
+        //    get { return currNumber; }
+        //    set
+        //    {
+        //        currNumber = value;
+        //        // Передаем в метку текущее значение
+        //        numberDisplay.Content = CurrentNumber.ToString();
+        //    }
+        //}
+
+
+        public int CurrentNumber
+        {
+            get { return (int)GetValue(CurrentNumberProperty); }
+            set { SetValue(CurrentNumberProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for CurrentNumber. This enables animation, styling, binding, etc...
+
+        public static bool ValidateCurrentNumber(object value)
+        {
+            if (Convert.ToInt32(value) >= 0 && Convert.ToInt32(value) <= 500)
+                return true;
+            else
+                return false;
+        }
+
+        private static void CurrentNumberChanged(DependencyObject depObj, DependencyPropertyChangedEventArgs args)
+        {
+            ShowNumberControl s = (ShowNumberControl)depObj;
+            Label theLabel = s.numberDisplay;
+            theLabel.Content = args.NewValue.ToString();
+        }
+    }
+}
